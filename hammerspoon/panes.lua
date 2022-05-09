84717e3dc8b20645359d6f201216ed0a37fef810
Janat Taerakul <janat.taerakul@gmail.com>
2022-05-10 01:54:42 +0900

    adds custom hyper apps, karabiner profiles

--- hammerspoon/panes.lua
+++ hammerspoon/panes.lua
@@ -10,24 +10,6 @@ local itermHotkeyMappings = {
     from = {{'ctrl', 'shift'}, '\\'},
     to   = {{'cmd'}, 'd'}
   },
-
-  -- Use control + h/j/k/l to move left/down/up/right by one pane
-  {
-    from = {{'ctrl'}, 'h'},
-    to   = {{'cmd', 'alt'}, 'left'}
-  },
-  {
-    from = {{'ctrl'}, 'j'},
-    to   = {{'cmd', 'alt'}, 'down'}
-  },
-  {
-    from = {{'ctrl'}, 'k'},
-    to   = {{'cmd', 'alt'}, 'up'}
-  },
-  {
-    from = {{'ctrl'}, 'l'},
-    to   = {{'cmd', 'alt'}, 'right'}
-  },
 }
 
 local terminalWindowFilter = hs.window.filter.new('iTerm2')
