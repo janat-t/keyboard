af54a78ec03a50ccb0c1f94d22d245aa9c683910
Janat Taerakul <jtaerakul@indeed.com>
2024-09-05 21:00:53 +0900

    Update profile change command

--- karabiner/current-profile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-MODE=$(grep selected karabiner.json | grep -n true | awk '{print $1}' | cut -d : -f 1)
-# echo $MODE
-grep -e '^\s\{12\}"name"' ./karabiner.json | grep -n name | grep $MODE | awk '{print $3}' | cut -d \" -f 2 | cat
